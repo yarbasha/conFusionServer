f9e0edcee8adc16bfa5fe525e9c44cbcaabab5e4
yarbasha <yarbasha@gmail.com>
2020-06-14 19:44:18 +0300

    Integrating React Client and Server

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -5,6 +5,7 @@ const authenticate = require('../authenticate');
 const cors = require('./cors');
 
 const Favorites = require('../models/favorite');
+const user = require('../models/user');
 
 const favoriteRouter = express.Router();
 
@@ -75,8 +76,27 @@ favoriteRouter.route('/')
 favoriteRouter.route('/:dishId')
   .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
   .get(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on /favorites/' + req.params.dishId);
+    Favorites.findOne({ user: req.user._id })
+      .then((favorites) => {
+        if (!favorites) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          return res.json({ exists: false, favorites: favorites });
+        }
+        else {
+          if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({ exists: false, favorites: favorites });
+          }
+          else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({ exists: true, favorites: favorites });
+          }
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err));
   })
   .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Favorites.findOne({ user: req.user._id })
